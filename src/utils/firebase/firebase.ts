679c6b0b95da92104c5984a8abb8a977de0a4e6e
karimWG7 <karimghanem871@gmail.com>
2024-04-06 06:20:11 +0200

    commenting the firebase code untill i use it

--- src/utils/firebase/firebase.ts
+++ src/utils/firebase/firebase.ts
@@ -1,13 +1,13 @@
-import { initializeApp } from "firebase/app";
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.VITE_FIREBASE_APP_ID,
-  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
-};
+// import { initializeApp } from "firebase/app";
+// const firebaseConfig = {
+//   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//   authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.VITE_FIREBASE_APP_ID,
+//   measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
